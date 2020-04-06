85375c8137d63e5d669147d72b869dc902bed3ab
Havard Fjaer <github@fjaer.com>
2020-04-06 13:33:00 +0200

    Cleaned up formatting, comments and readme.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # linuxcnc-midi
-LinuxCNC HAL module for mapping MIDI events to HAL pins
+[LinuxCNC](http://linuxcnc.org/) HAL module for mapping MIDI events to HAL pins. 
+
+Uses [pyrtmidi](https://github.com/patrickkidd/pyrtmidi) to pick up MIDI messages.
+
+## Compability
+Works with: 
+- Python 2.7 
+- LinuxCNC 2.7.x. 
+
+May work with other versions.
+
+## Install
+Install module using halcompile in bash: 
+```
+sudo halcompile --install --userspace midi.py
+```
+
+## Usage
+Load module in your .hal file: 
+```
+loadusr -W midi
+```
+
+### Mapping MIDI controller to GUI slider
+Set scale to a division of the max controller MIDI value of 127:
+```
+# Feed override
+setp halui.feed-override.scale 0.03937 # 5/127 
+net midi-feed-override-counts midi.1.controller.0.out => halui.feed-override.counts
+```
+
+### Mapping MIDI controller to buttons 
+Requires a conversion from S32 to BIT. (Not sure if `conv-s32-bit.0.clamp 1` is the best way, but that is what I got working.):
+```
+loadrt conv_s32_bit count=0
+addf conv-s32-bit.0 servo-thread
+
+# Run
+net midi-1-controller-41-active midi.1.controller.41.out => conv-s32-bit.0.in
+setp conv-s32-bit.0.clamp 1
+net midi-run conv-s32-bit.0.out-of-range => halui.program.run
+```
+
+### Identify MIDI controller ID's
+Run [midi_monitor.py](midi_monitor.py) to identfy what controller ID a physical slider or button is mapped to.
+
+### More examples
+See my LinuxCNC configuration repo for more examples: https://github.com/havard-fjaer/linuxcnc
+
+Particularly: https://github.com/havard-fjaer/linuxcnc/blob/master/configs/X-Carve/custom_postgui.hal
--- midi.py
+++ midi.py
@@ -1,63 +1,69 @@
 #!/usr/bin/env python
-#Install with: sudo halcompile --install --userspace midi.py 
+# Install with: sudo halcompile --install --userspace midi.py
 import sys
 import rtmidi
 import threading
-import hal, time
+import hal
+import time
 halMidi = hal.component('midi')
-
 debug = False
-
-
 class Collector(threading.Thread):
     def __init__(self, device, port, hal):
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.port = port
         self.portName = device.getPortName(port)
-        self.device = device
+        self.midiDevice = device
         self.hal = hal
         self.quit = False
 
     def run(self):
-        self.device.openPort(self.port)
-        self.device.ignoreTypes(True, False, True)
+        self.midiDevice.openPort(self.port)
+        self.midiDevice.ignoreTypes(True, False, True)
         while True:
-           if self.quit:
+            if self.quit:
                 return
-           midiMessage = self.device.getMessage()
-           if midiMessage and midiMessage.isController():
+            # Read any available MIDI message
+            midiMessage = self.midiDevice.getMessage()
+            if midiMessage and midiMessage.isController():
                 controllerNumber = midiMessage.getControllerNumber()
                 controllerValue = midiMessage.getControllerValue()
                 if debug:
                     print(controllerNumber, controllerValue)
-                self.hal[str(self.port) + '.controller.'+ str(controllerNumber) + '.out']  = controllerValue 
+                # Map MIDI message to HAL pin
+                self.hal[str(self.port) + '.controller.' + str(controllerNumber) + '.out'] = controllerValue
+
 
 try:
     midiIn = rtmidi.RtMidiIn()
     collectors = []
-
     
+    # Go through all MIDi devices
     for port in range(midiIn.getPortCount()):
-        midiDevice = rtmidi.RtMidiIn()    
-        midiPortName = midiIn.getPortName(port)
+        midiDevice = rtmidi.RtMidiIn()
         if debug:
-            print(midiPortName)
+            print(midiIn.getPortName(port))
+
         collector = Collector(midiDevice, port, halMidi)
-        
         collector.start()
         collectors.append(collector)
 
+        # Add HAL pins for all possible MIDI controllers on each device
+        # (I don't see how I would identify what controllers that actually are present on a device)
         for controller in range(0, 127):
             halPinName = str(port) + ".controller." + str(controller) + '.out'
             if debug:
                 print(halPinName)
             halMidi.newpin(halPinName, hal.HAL_S32, hal.HAL_OUT)
     halMidi.ready()
-    while True: 
+
+    # Wait
+    while True:
         time.sleep(1)
 
+# Clean up
+finally:
     for c in collectors:
         c.quit = True
-finally:
+    # If HAL is not closed, the component might stand in the way when trying to spin up the script again
     halMidi.exit()
